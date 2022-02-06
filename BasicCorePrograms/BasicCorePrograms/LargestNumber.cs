c772ed347c7656b822e32822bc471de38f5438bf
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-06 14:49:17 +0530

    [kunal]commiting changes for LargestNumber program

---
+++ BasicCorePrograms/BasicCorePrograms/LargestNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class LargestNumber
+    {
+        public void FindLargestNumber()
+        {
+            int a, b, c;
+            Console.WriteLine("largest among three numbers:");
+            //reading inputs
+            Console.WriteLine("Enter value for a :");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for c:");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            //finding largest among three numbers
+            if ((a > b) && (a > c))
+            {
+                Console.WriteLine("{0} is largest number", a);
+            }
+            else if ((b > a) && (b > c))
+            {
+                Console.WriteLine("{0} is largest number", b);
+            }
+            else if ((c > a) && (c > b))
+            {
+                Console.WriteLine("{0} is largest number", c);
+            }
+            else
+                Console.WriteLine("{0} is constat number", a, b, c);
+            {
+
+            }
+            }
+        }
+}
--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -2,11 +2,14 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-EvenorOdd evenodd = new EvenorOdd();
-evenodd.CheckEvenorOdd();
+//EvenorOdd evenodd = new EvenorOdd();
+//evenodd.CheckEvenorOdd();
 
-FlipCoin percent = new FlipCoin();
-percent.FlipCoinPercentage();
+//FlipCoin percent = new FlipCoin();
+//percent.FlipCoinPercentage();
 
-HarmonicNumber number = new HarmonicNumber();
-number.FindHarmonicNumber();
+//HarmonicNumber number = new HarmonicNumber();
+//number.FindHarmonicNumber();
+
+LargestNumber largest = new LargestNumber();
+largest.FindLargestNumber();
