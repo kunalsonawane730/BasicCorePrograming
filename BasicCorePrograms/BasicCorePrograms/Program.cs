c20bd6942821415e358af4968d804ca5a4644d0f
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-06 14:37:13 +0530

    [kunal]commiting changes for EvenorOdd program

---
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -0,0 +1,6 @@
+﻿using BasicCorePrograms;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+EvenorOdd evenodd = new EvenorOdd();
+evenodd.CheckEvenorOdd();
